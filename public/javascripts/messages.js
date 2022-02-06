b90b9b633c75788573ebae3e6c56de5d0a02eefc
anthony <anthonydwan@outlook.com>
2022-02-06 23:41:06 +1100

    fixed the display of time

--- public/javascripts/messages.js
+++ public/javascripts/messages.js
@@ -1,15 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const changeTime = (hr, min) => {
+    let d = new Date();
+    d.setHours(d.getHours() - hr);
+    d.setMinutes(d.getMinutes() - min);
+    return d;
+};
 const messages = [
     {
         text: "Obviously Javascript is the best language ever created.",
         user: "Amando",
-        added: new Date(),
+        added: changeTime(2, 13),
     },
     {
         text: "Hello Everyone! I just started learning from the Odin Project, nice to meet you all",
         user: "Charles",
-        added: new Date(),
+        added: changeTime(5, 34),
     },
 ];
 exports.default = messages;
--- public/javascripts/messages.ts
+++ public/javascripts/messages.ts
@@ -4,16 +4,23 @@ interface message {
   added: Date;
 }
 
+const changeTime = (hr: number, min: number) => {
+  let d = new Date();
+  d.setHours(d.getHours() - hr);
+  d.setMinutes(d.getMinutes() - min);
+  return d;
+};
+
 const messages: message[] = [
   {
     text: "Obviously Javascript is the best language ever created.",
     user: "Amando",
-    added: new Date(),
+    added: changeTime(2, 13),
   },
   {
     text: "Hello Everyone! I just started learning from the Odin Project, nice to meet you all",
     user: "Charles",
-    added: new Date(),
+    added: changeTime(5, 34),
   },
 ];
 
